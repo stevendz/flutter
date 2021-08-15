eea6fb4dcddf82f5d3da654825ca3e906e67c593
stevendz <steven.dzionara@gmail.com>
2021-08-15 22:57:21 +0200

    Revert "Add maxWidth to ToolTip and ToolTipTheme"
    This reverts commit 807b12c8767cbf9771027ef0a788bd58cdc961c5.

--- packages/flutter/lib/src/material/tooltip_theme.dart
+++ packages/flutter/lib/src/material/tooltip_theme.dart
@@ -38,6 +38,8 @@ class TooltipThemeData with Diagnosticable {
     this.textStyle,
     this.waitDuration,
     this.showDuration,
+    this.triggerMode,
+    this.enableFeedback,
   });
 
   /// The height of [Tooltip.child].
@@ -88,6 +90,22 @@ class TooltipThemeData with Diagnosticable {
   /// The length of time that the tooltip will be shown once it has appeared.
   final Duration? showDuration;
 
+  /// The [TooltipTriggerMode] that will show the tooltip.
+  final TooltipTriggerMode? triggerMode;
+
+  /// Whether the tooltip should provide acoustic and/or haptic feedback.
+  ///
+  /// For example, on Android a tap will produce a clicking sound and a
+  /// long-press will produce a short vibration, when feedback is enabled.
+  ///
+  /// This value is used if [Tooltip.enableFeedback] is null.
+  /// If this value is null, the default is true.
+  ///
+  /// See also:
+  ///
+  ///   * [Feedback], for providing platform-specific feedback to certain actions.
+  final bool? enableFeedback;
+
   /// Creates a copy of this object but with the given fields replaced with the
   /// new values.
   TooltipThemeData copyWith({
@@ -101,6 +119,8 @@ class TooltipThemeData with Diagnosticable {
     TextStyle? textStyle,
     Duration? waitDuration,
     Duration? showDuration,
+    TooltipTriggerMode? triggerMode,
+    bool? enableFeedback,
   }) {
     return TooltipThemeData(
       height: height ?? this.height,
@@ -114,6 +134,8 @@ class TooltipThemeData with Diagnosticable {
       textStyle: textStyle ?? this.textStyle,
       waitDuration: waitDuration ?? this.waitDuration,
       showDuration: showDuration ?? this.showDuration,
+      triggerMode: triggerMode ?? this.triggerMode,
+      enableFeedback: enableFeedback ?? this.enableFeedback,
     );
   }
 
@@ -152,6 +174,8 @@ class TooltipThemeData with Diagnosticable {
       textStyle,
       waitDuration,
       showDuration,
+      triggerMode,
+      enableFeedback
     );
   }
 
@@ -189,6 +213,8 @@ class TooltipThemeData with Diagnosticable {
     properties.add(DiagnosticsProperty<TextStyle>('textStyle', textStyle, defaultValue: null));
     properties.add(DiagnosticsProperty<Duration>('wait duration', waitDuration, defaultValue: null));
     properties.add(DiagnosticsProperty<Duration>('show duration', showDuration, defaultValue: null));
+    properties.add(DiagnosticsProperty<TooltipTriggerMode>('triggerMode', triggerMode, defaultValue: null));
+    properties.add(FlagProperty('enableFeedback', value: enableFeedback, ifTrue: 'true', showName: true, defaultValue: null));
   }
 }
 
@@ -258,4 +284,27 @@ class TooltipTheme extends InheritedTheme {
 
   @override
   bool updateShouldNotify(TooltipTheme oldWidget) => data != oldWidget.data;
+}
+
+/// The method of interaction that will trigger a tooltip.
+/// Used in [Tooltip.triggerMode] and [TooltipThemeData.triggerMode].
+enum TooltipTriggerMode {
+  /// Tooltip will only be shown by calling `ensureTooltipVisible`.
+  manual,
+
+  /// Tooltip will be shown after a long press.
+  ///
+  /// See also:
+  ///
+  ///   * [GestureDetector.onLongPress], the event that is used for trigger.
+  ///   * [Feedback.forLongPress], the feedback method called when feedback is enabled.
+  longPress,
+
+  /// Tooltip will be shown after a single tap.
+  ///
+  /// See also:
+  ///
+  ///   * [GestureDetector.onTap], the event that is used for trigger.
+  ///   * [Feedback.forTap], the feedback method called when feedback is enabled.
+  tap,
 }
